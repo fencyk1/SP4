24efab59f2904a9c2f182f6b66063c77fd8a60bb
fencyk1 <fencyk.1@buckeyemail.osu.edu>
2010-11-10 11:00:53 -0500

    The beginnings of a skeleton.

---
+++ src/ObjectIn.java
@@ -0,0 +1,13 @@
+import java.io.File;
+import java.io.IOException;
+
+
+public class ObjectIn implements ObjectInInterface{
+
+	@Override
+	public void readObjectFile(File object) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/ObjectInInterface.java
@@ -0,0 +1,11 @@
+import java.io.File;
+import java.io.IOException;
+
+
+
+
+public interface ObjectInInterface {
+
+	void readObjectFile(File object) throws IOException;
+	
+}
